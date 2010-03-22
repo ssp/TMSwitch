915d00070c8a29cb497c6d7a1e298b10555fe2ad
ssp <ssp-web@earthlingsoft.net>
2010-03-22 10:32:50 +0100

    Roll Time Machine toggling into the tool.
    Currently hard coded to wake at 8:08 and sleep at 22:30.

--- TMSwitch.m
+++ TMSwitch.m
@@ -37,6 +37,10 @@ NSString * const TMSVolumeUUIDsKey = @"volumeUUIDs";
 NSString * const TMDefaultsFilePath = @"/Library/Preferences/com.apple.TimeMachine";
 NSString * const TMDefaultsAliasDataKey = @"BackupAlias";
 NSString * const TMDefaultsUUIDKey = @"DestinationVolumeUUID";
+NSString * const TMDefaultsAutoBackupKey = @"AutoBackup";
+
+NSString * const TMBackupdHelperPath = @"/System/Library/CoreServices/backupd.bundle/Contents/Resources/backupd-helper";
+
 
 
 NSString * UUIDForVolumeURL ( NSURL * volumeURL ) {
@@ -117,35 +121,95 @@ NSData * aliasDataForVolumeURL ( NSURL * URL ) {
 
 
 
-int main (int argc, const char * argv[]) {
+BOOL switchVolume () {
+	BOOL switchedVolume = NO;
+	
+	NSUserDefaults * userDefaults = [NSUserDefaults standardUserDefaults];
+	NSString * currentVolumeUUID = [[userDefaults persistentDomainForName:TMDefaultsFilePath] objectForKey: TMDefaultsUUIDKey];
+	
 	NSArray * volumeURLs = [[NSFileManager defaultManager] mountedVolumeURLsIncludingResourceValuesForKeys:nil options:NSVolumeEnumerationSkipHiddenVolumes];
-		
-	if ( argc == 1 ) {
-		// no paramters, just switch
-		for ( NSURL * volumeURL in volumeURLs ) {
-			NSString * volumeUUID = UUIDForVolumeURL( volumeURL );
-			if ( isQualifiedVolumeUUID(volumeUUID) ) {
-				NSString * currentVolumeUUID = [[[NSUserDefaults standardUserDefaults] persistentDomainForName:TMDefaultsFilePath] objectForKey: TMDefaultsUUIDKey];
-				if ( ! [volumeUUID isEqualToString: currentVolumeUUID ] ) {
-					NSData * aliasData = aliasDataForVolumeURL( volumeURL );
-					
-					NSUserDefaults * userDefaults = [NSUserDefaults standardUserDefaults];
-					NSDictionary * defaults = [[userDefaults persistentDomainForName: TMDefaultsFilePath] mutableCopy];
-					[defaults setValue: aliasData forKey: TMDefaultsAliasDataKey];
-					[defaults setValue: volumeUUID forKey: TMDefaultsUUIDKey];
-					
-					[userDefaults setPersistentDomain: defaults forName: TMDefaultsFilePath];
-					[userDefaults synchronize];
-					
-					NSString * message = [NSString stringWithFormat:@"New Time Machine volume is: %@.\n", [volumeURL path]];
-					const char * theMessage = [message cStringUsingEncoding:NSUTF8StringEncoding];
-					fprintf(stderr, theMessage);					
-				}
-				else {
-					fprintf(stderr, "Time Machine volume does not need to be changed.\n");					
-				}
-				break;
+
+	for ( NSURL * volumeURL in volumeURLs ) {
+		NSString * volumeUUID = UUIDForVolumeURL( volumeURL );
+		if ( isQualifiedVolumeUUID(volumeUUID) ) {
+			
+			if ( ! [volumeUUID isEqualToString: currentVolumeUUID ] ) {
+				NSData * aliasData = aliasDataForVolumeURL( volumeURL );
+				
+				NSDictionary * defaults = [[userDefaults persistentDomainForName: TMDefaultsFilePath] mutableCopy];
+				[defaults setValue: aliasData forKey: TMDefaultsAliasDataKey];
+				[defaults setValue: volumeUUID forKey: TMDefaultsUUIDKey];
+				
+				[userDefaults setPersistentDomain: defaults forName: TMDefaultsFilePath];
+				
+				NSString * message = [NSString stringWithFormat:@"New Time Machine volume is: %@.\n", [volumeURL path]];
+				NSLog(@"%@", message);
+				switchedVolume = YES;
 			}
+			else {
+				// NSLog(@"Time Machine volume does not need to be changed.");
+			}
+			
+			break;
+		}
+	}
+	[userDefaults synchronize];
+	
+	return switchedVolume;
+}
+
+
+
+BOOL toggleTimeMachine () {
+	BOOL toggled = NO;
+	
+	NSDate * now = [NSDate date];
+	NSDictionary * locale =  [[NSUserDefaults standardUserDefaults] dictionaryRepresentation];
+	NSString * timeZone = [now descriptionWithCalendarFormat:@"%z" timeZone:nil locale:locale];
+	NSString * date = [now descriptionWithCalendarFormat:@"%Y-%m-%d" timeZone:nil locale:locale];
+	
+	NSDate * turnOnTime = [NSDate dateWithString: [NSString stringWithFormat:@"%@ 08:08:08 %@", date, timeZone]];
+	NSDate * turnOffTime = [NSDate dateWithString: [NSString stringWithFormat:@"%@ 22:30:00 %@", date, timeZone]];
+	
+	BOOL desiredStatus = NO;
+	if ( [turnOnTime compare:now] == NSOrderedAscending  && [turnOffTime compare:now] == NSOrderedDescending ) {
+		desiredStatus = YES;
+	}
+	
+	NSUserDefaults * userDefaults = [NSUserDefaults standardUserDefaults];
+	NSDictionary * TMDefaults = [userDefaults persistentDomainForName: TMDefaultsFilePath];
+	
+	const BOOL actualStatus = [[TMDefaults objectForKey: TMDefaultsAutoBackupKey] boolValue];
+	
+	if ( actualStatus != desiredStatus ) {
+		NSMutableDictionary * myTMDefaults = [TMDefaults mutableCopy];
+		[myTMDefaults setObject: [NSNumber numberWithBool: desiredStatus] forKey: TMDefaultsAutoBackupKey];
+		[userDefaults synchronize];
+
+		// check whether our modification made it:
+		const BOOL newStatus = [[[[NSUserDefaults standardUserDefaults] persistentDomainForName: TMDefaultsFilePath] objectForKey: TMDefaultsAutoBackupKey] boolValue];
+		NSString * newStatusWord = desiredStatus ? @"ON" : @"OFF";
+
+		if ( newStatus == desiredStatus ) {
+			toggled = YES;
+			NSLog(@"Turned Time Machine %@", newStatusWord);
+		}
+		else{
+			NSLog(@"Failed to turn Time Machine %@.", newStatusWord);
+		}
+	}
+	
+	return toggled;
+}
+
+
+
+int main (int argc, const char * argv[]) {
+	if ( argc == 1 ) {
+		// no paramters, just switch the volume and change Time Machine's status
+		if ( switchVolume() || toggleTimeMachine() ) {
+			// defaults were changed: run Time Machine
+			[NSTask launchedTaskWithLaunchPath:TMBackupdHelperPath arguments:[NSArray arrayWithObject:@"-auto"]];
 		}
 	}
 	else if ( argc == 2 ) {
